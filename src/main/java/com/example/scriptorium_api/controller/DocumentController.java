a37ee325de7796d3bfbf8cefb76c1746debfc27e
chrisyoung0101 <chrisyoung0101@gmail.com>
2025-02-05 19:15:59 -0600

    more debug CORS

--- src/main/java/com/example/scriptorium_api/controller/DocumentController.java
+++ src/main/java/com/example/scriptorium_api/controller/DocumentController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/documents")
-//@CrossOrigin(origins = "https://scriptorium-ui.netlify.app", allowedHeaders = "*", allowCredentials = "true")
+@CrossOrigin(origins = "https://scriptorium-ui.netlify.app", allowedHeaders = "*", allowCredentials = "true")
 public class DocumentController {
 
     @Autowired
