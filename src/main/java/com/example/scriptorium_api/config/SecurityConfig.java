5b2ae2c6f0df7de32dc562d8d1253b82e5ec4044
chrisyoung0101 <chrisyoung0101@gmail.com>
2025-02-08 08:11:19 -0600

    more debug CORS

--- src/main/java/com/example/scriptorium_api/config/SecurityConfig.java
+++ src/main/java/com/example/scriptorium_api/config/SecurityConfig.java
@@ -15,10 +15,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
